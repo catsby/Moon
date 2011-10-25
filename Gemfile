105c0fe57abfe9c5fd0f496b537470300d605283
Clint Shryock <clint@ctshryock.com>
2011-10-24 21:43:59 -0500

    Show buckets, then keys Refactor some Use Haml haml views

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'ripple'
+gem 'haml'
 gem 'riak-sessions'
--- sinatra_riak.rb
+++ sinatra_riak.rb
@@ -1,13 +1,27 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'riak'
+require 'haml'
 
 
 class SinatraRiak < Sinatra::Base
   enable :sessions
+  
+  set :haml, :format => :html5
+
+  before do 
+    @client = Riak::Client.new(:port => 8091)
+  end
 
   get '/' do
-    @message = 'Riak World!'
+    @buckets = @client.buckets
+    haml :index
+  end
+
+  get '/bucket/:name' do |name|
+    @bucket = @client.bucket(name)
+    @keys = @bucket.keys
+    haml :bucket
   end
 
 end
